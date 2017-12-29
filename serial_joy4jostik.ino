09dd265b88f41744b268664ac141fd2dda200a79
woronin <woronin05@yandex.ru>
2017-12-29 15:03:32 +0400

    Update serial_joy4jostik.ino

--- serial_joy4jostik.ino
+++ serial_joy4jostik.ino
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////
-//     2017.12.24 woronin,  umkiedu@gmail.com     
+//     2017.12.29 woronin,  umkiedu@gmail.com     
 //     Robot UMKI controller K6_2_mini
 //     To connect using 4joyjostik mobile app by link http://arduino-robot.site/basic/serial                 
 //     - for ANDROID 4.0.1 or later version; 
@@ -12,12 +12,13 @@ int byte_bakward[]={0, 0, 127, 0, 4, 0, 0};
 int byte_left[]=   {0, 129, 0, 0, 4, 0, 0};
 int byte_right[]=  {0, 127, 0, 0, 4, 0, 0};
 int byte_stop[]=   {0, 0, 0, 0, 4, 0, 0};
+int byte_start[]=   {0, 0, 0, 0, 4, 0, 2};
 int m1speed=3; // скорость левый
 int m2speed=5; // скорость правый
 int m1direction=2; // направление правый
 int m2direction=4; // направление левый
 
-int side, pwm=255;
+int side, pwm=255, program_move[25];
 
 
 void setup() {
@@ -30,36 +31,47 @@ void loop() // выполняется циклически записываем 
 {
 
   int inByte[7], i;
+  static int n=0;
   if (BTSerial.available()){
      if (BTSerial.read()==0){
       for (i = 0; i< 7; i++) {
         inByte[i] = BTSerial.read();   
-        Serial.print(inByte[i]); 
-        Serial.print(i); 
+        delay(50);
+ //       Serial.print(inByte[i]); 
+ //       Serial.print(i); 
       } 
    
   if (inByte[1]==byte_forward[1] && inByte[2]==byte_forward[2]) {
-     Serial.println(" 8888 ");
      go_forward(pwm);
+     program_move[n]=0x2; //запоминеаем в программе шаг вперед
+     n++;
   }
   if (inByte[1]==byte_bakward[1] && inByte[2]==byte_bakward[2]) {
-     Serial.println(" 9999 ");
      go_back(pwm);
+     program_move[n]=0xc; //запоминеаем в программе шаг назад
+     n++;
   }
   if (inByte[1]==byte_left[1] && inByte[2]==byte_left[2]) {
-     Serial.println(" 7777 ");
      go_left(pwm);
+     program_move[n]=0x4; //запоминеаем в программе шаг влево
+     n++;   
   }
   if (inByte[1]==byte_right[1] && inByte[2]==byte_right[2]) {
-     Serial.println(" 6666 ");
-      go_right(pwm);
+     go_right(pwm);
+     program_move[n]=0x8; //запоминеаем в программе шаг вправо
+     n++;     
   }     
   if (inByte[1]==byte_stop[1] && inByte[2]==byte_stop[2]) {
       Serial.println(" 0000 ");   
-      analogWrite(m1speed, 0); // скорость стоп
-      analogWrite(m2speed, 0);
+      go_stop(pwm);
   }
+  if (inByte[4]==byte_start[4] && inByte[6]==byte_start[6]) {
+      Serial.println(" 1111 ");   
+      go_program (pwm); // запускаем езду по программе
+      n=0;
+  }  
              }
+  if (n>=25) n=0;
   }
 
 
@@ -96,4 +108,41 @@ digitalWrite(m1direction,HIGH ); // назад правый
 digitalWrite(m2direction,LOW); // вперед левый
 analogWrite(m1speed, pwm); // скорость
 analogWrite(m2speed, pwm);
+}
+
+void go_stop(int pwm)
+{
+      analogWrite(m1speed, 0); // скорость стоп
+      analogWrite(m2speed, 0);
+}
+
+void go_program(int pwm)
+{
+  int n;
+ for (n = 0; n< 25; n++) {
+   Serial.print(n); 
+   Serial.print(" = ");    
+   Serial.println(program_move[n]); 
+   if (program_move[n]==0x2) {
+     go_forward(pwm);
+     delay(500);
+     go_stop(pwm);
+  }
+  else if (program_move[n]==0x4) {
+     go_left(pwm);
+     delay(500);
+     go_stop(pwm);
+  }
+  else if (program_move[n]==0x8) {
+     go_right(pwm);
+     delay(500);
+     go_stop(pwm);
+  }
+  else if (program_move[n]==0xc) {
+     go_back(pwm);
+     delay(500);
+     go_stop(pwm);
+  }
+
+ }
 }
